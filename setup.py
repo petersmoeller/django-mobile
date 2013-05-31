a46adfbd416610480cee8d5a972aa47744ab704c
Peter Moeller <peter@webloftet.dk>
2013-05-31 09:42:29 +0200

    temp removed tests from setup.py
    Signed-off-by: Peter Moeller <peter@webloftet.dk>

--- setup.py
+++ setup.py
@@ -56,6 +56,6 @@ setup(
         'django_mobile.cache',
     ],
     install_requires = ['setuptools'],
-    tests_require = ['Django', 'mock'],
+    #tests_require = ['Django', 'mock'],
     #test_suite = 'django_mobile_tests.runtests.runtests',
 )
